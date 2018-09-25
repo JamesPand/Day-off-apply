c0f9531c5e71cf45ccbf5bfb683b783996ae612d
JamesPand <43084114+JamesPand@users.noreply.github.com>
2018-09-25 09:52:29 +0800

    Add files via upload

--- restsystem/abilities.php
+++ restsystem/abilities.php
@@ -6,6 +6,7 @@
  echo '<a href="verify.php">待審核</a> ';
  echo '<a href="verify_history.php">審核記錄</a> ';
  echo '<a href="new_user.php">使用者管理</ a> ';
+ echo '<a href="groupmail.php">群組發信</ a> ';
  echo '<a href="logout.php">登出</a> ';
  echo '</div>';
  
